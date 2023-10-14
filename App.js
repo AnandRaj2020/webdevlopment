51a29b85f35fda3a621fa5f6705282eb80a57a11
ANAND RAJ <83631626+AnandRaj2020@users.noreply.github.com>
2023-10-14 23:44:05 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import Navbar from "./components/navbar";
+import Intro from "./components/intro";
+import Skills from "./components/skills";
+
+
+import Footer from "./components/footer";
+
+
+function App() {
+  return (
+    <div className="App">
+   <Navbar/>
+   <Intro/>
+   <Skills/>
+  
+   <Footer/>
+   
+   </div>
+  );
+}
+
+export default App;
